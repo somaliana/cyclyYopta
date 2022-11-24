6437e4a344fc0d9478ffc8cb561b08508123c451
Your Name <somaliana12@yandex.ru>
2022-11-24 13:17:19 +0300

    yoba

--- six.c
+++ six.c
@@ -3,19 +3,26 @@
 
 int main() {
     int n;
-    double x;
-    scanf("%d %lf", &n, &x);
+    double x,e;
+    scanf("%d %lf %lf", &n, &x, &e);
     double s0 = 0;
     double s1 = 0.5;
     int i = 2; 
     
-    while (i<n) {
+    do {
+        if (i > n) {
+            printf("Whoops");
+            break;
+
+        }
         int copyThat = s1;
 
         s1 = x*((s0+s1)/2) + s0*(x*x);
         s0 = copyThat;
         i++;
     }
+    while (fabs(s0-s1) > e);
+
     printf("%lf",s1);
 
 }
--- three.c
+++ three.c
@@ -2,16 +2,23 @@
 #include <math.h> 
 
 int main() {
-    double x;
+    double x,e;
     int k; 
     
-    scanf("%lf %d", &x, &k);
+    scanf("%lf %lf %d", &x,&e,&k);
     double rec= (x*x)/2;
-    int i = 1;
-    while (i <= k) {
+    double previous;
+    int i = 0;
+    do {
+        if (i>k) {
+            printf("Yoops");
+            break;
+        }
+        previous = rec;
         rec *= ((x*x)/2)/((k+1)*(k+1)*(2*k+1));
         i++;
     }
+    while (fabs(rec-previous) > e);
     printf("%lf", rec);
 
 
