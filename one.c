3393ccff12d41bcc3940fa129e85dd4b3899735a
Your Name <somaliana12@yandex.ru>
2022-11-28 16:02:13 +0300

    ewqq

--- one.c
+++ one.c
@@ -5,20 +5,16 @@ int main() {
     int n; 
     scanf("%d", &n);
     double firstSum, secondSum; 
-    firstSum = 1;
-    secondSum = 1;
-
+    firstProd = 1;
+    secondSum = 0;
+    int factor = 1;
     int i = 1;
     while (i <= n) {
-        firstSum = firstSum * (1 + 1/(sqrt(i)));
+        firstProd = firstProd* (1 + 1/(sqrt(i)));
         int factor = 1; 
         int k = 1;
-        while (k<=i) {
-            factor = factor*k;
-            k += 1;
-
-        }
-        secondSum = secondSum*(1 + 1/factor);
+        factor *= i;
+        secondSum += (1 + 1/factor);
         i += 1;
     }
     printf("%f",firstSum * secondSum);
